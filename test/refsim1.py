cac63b4a0ab881e35ca3961dadaf6797bed9d9d7
David C Sterratt <david.c.sterratt@ed.ac.uk>
2013-09-27 17:11:02 +0100

    Modified reference simulations

--- test/refsim1.py
+++ test/refsim1.py
@@ -1,4 +1,9 @@
 from test_ca_pulse_common import *
 
+compare_traces(diam=0.7, gcalbar=0.05, 
+               gamma2=1, P0=0.2)
+
 compare_traces(diam=0.2, gcalbar=0.05, 
                gamma2=1, P0=0.2)
+
+
