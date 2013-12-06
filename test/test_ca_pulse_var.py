2a8971e56d00e6700c45f4708d91baac2f0bf2a3
David C Sterratt <david.c.sterratt@ed.ac.uk>
2013-12-06 16:40:34 +0000

    Added files in which I'll try using molar parameters

---
+++ test/test_ca_pulse_var.py
@@ -0,0 +1,32 @@
+## Spine head into which Ca flows via a very simple channel and is
+## pumped out.  Voltage clamp ensures almost constant Ca flow.
+from test_ca_pulse_common import *
+from neuron import rxd
+
+def run(diam=0.2, 
+        gcalbar=0.05,
+        gamma2=1,
+        P0=0.2):
+    # Neuron spine head
+    sh = make_spine_head(diam=diam, gcalbar=gcalbar)
+
+    ## Reaction-diffusion mechanism
+    ## This appears to integrate the incoming Ca
+    # WHERE the dynamics will take place
+    r = rxd.Region([sh], nrn_region='i')
+
+    # WHO are the actors
+    ca = rxd.Species(r, name='ca', charge=2, initial=0.0)
+    P  = rxd.Species(r, name='P',  charge=0, initial=P0)
+    kappa = rxd.Kappa([ca, P], "caPump.ka", r)
+    vol = sh.L*numpy.pi*(sh.diam/2)**2
+    kappa.setVariable('gamma1', 1E-3*0.1*numpy.pi*0.5**2/vol)
+    kappa.setVariable('gamma2', gamma2)
+
+    stim = insert_vclamp(sh)
+
+    ## Record P from spine head
+    rec_Pi = h.Vector()
+    rec_Pi.record(sh(0.5)._ref_Pi)
+
+    run_and_save(sh, rec_Pi, 'test_ca_pulse')
