7638abca4d4a70ea72524098372ee4c0d6f2b1d4
David C Sterratt <david.c.sterratt@ed.ac.uk>
2013-12-17 22:01:30 +0000

    Got combined Urbanak-Pepke model working

---
+++ demo/nmda_urakubo.mod
@@ -0,0 +1,133 @@
+COMMENT
+
+AMPA synapse with two decay time constants based on the exp2syn.mod
+mechanism from the NEURON distribution.
+
+We use the time constants measured by Andrasfalvy & Magee (2001) from
+patches excised from different parts of the stratum radiatum at
+22\degC.  These are in broad agreement with the time constants measured
+by Spruston &al (1995), epecially when the age-dependent change in
+receptor properties (Hestrin &al., 1992) is taken into account.
+Although they are considerably slower than the time constants measured
+using whole-cell recordings from hippocampal cells (Hestrin & al., 1990),
+this might be accounted for by the more depolarised potentials used in
+these recordings.  We corrected the time constants recorded at 22\degC
+to 34\degC using a \Qten of 3 (Hestrin & al., 1990).  This gave a
+rise time of 1.7\ms and a dual exponential decay comprising a fast
+component fast time constant of 67\ms and a slow component with a time
+constant of 428\ms in the ratio 0.61 to 0.39. 
+
+References
+
+B K Andr\'asfalvy & J C Magee 2001 "Distance-Dependent Increase in
+  AMPA Receptor Number in the Dendrites of Adult Hippocampal CA1
+  Pyramidal Neurons" J. Neurosci. 21, 9151-9159
+
+S Hestrin & al. 1990 "Mechanisms Generating the time course of dual
+  component excitatory synaptic currents recorded in hippocampal
+  slices" Neuron 5, 247-253
+
+S Hestrin 1992 "Developmental regulation of NMDA receptor-mediated
+  currents at a central synapse" Nature 357, 686-689
+
+N Spruston & al. 1995 "Dendritic glutamate receptor channels in rat
+  hippocampal CA3 and CA1 neurons" J. Physiol. 482, 325-352
+
+ENDCOMMENT
+
+NEURON {
+    POINT_PROCESS NmdaSynUrak
+    RANGE  e, g, i, b, ica, iGlu
+    NONSPECIFIC_CURRENT i
+    USEION ca READ cai,cao WRITE ica
+    USEION NMDA READ NMDAi,iNMDA VALENCE 0
+    USEION Glu  WRITE iGlu VALENCE 0
+    GLOBAL total, mg, q10, taurise, taufast, tauslow, taurise_exp, taufast_exp, tauslow_exp, afast, aslow, normfac, T_exp, K0, delta, fracca
+}
+
+INCLUDE "units.inc"
+
+PARAMETER {
+    : Time constants and afast from Andrasfalvy & Magee 01
+    taurise_exp =    6.46 (ms) <1e-9,1e9>    : rise      
+    taufast_exp =  252.5  (ms) <1e-9,1e9>    : fast decay
+    tauslow_exp = 1660    (ms) <1e-9,1e9>    : slow decay
+    afast = 0.61 <0,1>
+    e=0	(mV)
+    mg	= 1    (mM)		: external magnesium concentration
+    fracca= 0.13        : fraction of current that is ca ions; Srupuston &al 95
+    z = 2
+    celsius = 22	(degC)
+    T_exp = 22    (degC)
+    q10 = 3       : Hestrin 90
+    K0 = 4.1 (mM) : From Spruston &al 95
+    delta = 0.8   : From Spruston &al 95
+    N_A = 6.02205E23 
+}
+
+ASSIGNED {
+    v       (mV)
+    i       (nA)
+    ica	    (nA) 	
+    iGlu    (nA)
+    iNMDA   (nA)
+    g       (uS)
+    aslow 
+    total   (uS)
+    cai     (mM)
+    cao     (mM)
+    taurise (ms)
+    taufast (ms)
+    tauslow (ms)
+    normfac 
+    b
+    NMDAi  (mM)
+    vol  (micrometer3)
+    area (micrometer2)
+    diam (micrometer)
+    L    (micrometer)
+    conv  (/mM)
+    fGlu
+}
+
+INITIAL {
+    L = area/(PI*diam)
+    vol = L*PI*(diam/2)^2
+    conv = N_A*vol*(1e-18)
+}
+
+
+BREAKPOINT {
+    b = mgblock(v)		: b is the block by magnesium at this voltage
+    : Single channe conductnace * conversion factor * concentration of NMDAi * block factor
+    g = 0.045E-3(uS) *conv * NMDAi * b 
+    iGlu = fGlu*-0.0001
+    : correction for area of spine, assuming that the synapse is on a
+    : spine
+    i =   L/(L + diam/4) * g * (1-fracca) * (v - e) - iGlu
+    ica = L/(L + diam/4) * g * fracca     * ghkg(v,cai,cao,z)
+    : Should print out a near-integer number of open NMDA gates
+    : printf("NMDAi=%g\n", conv * NMDAi)
+}
+
+NET_RECEIVE(weight (uS)) {
+    if (flag == 0) {
+        fGlu = 1
+        net_send(0.1, 1)
+    } else {
+        fGlu = 0
+    }
+}
+
+INCLUDE "ghk.inc"
+
+FUNCTION mgblock(v(mV)) {
+    TABLE 
+    DEPEND mg, K0, delta, celsius
+    FROM -140 TO 80 WITH 1000
+    : From Spruston &al 95
+    mgblock = 1/(1+(mg/K0)*exp(-delta*z*FARADAY*v*(0.001)/R/(celsius+273)))
+}
+
+
+
---
+++ demo/simple-psd-pepke-kappa-nmda.py
@@ -0,0 +1,230 @@
+## Spine head into which Ca flows via an NMDA channel and is pumped
+## out. Voltage clamp ensures almost constant Ca flow.
+from neuron import *
+from neuron import rxd
+import numpy
+
+## Time for equilibriation in seconds
+t_equil = 1
+
+## Neuron
+
+h.celsius = 34                  # Temperature
+g_pas = 0.0001               # Gives membrane time constant of 10ms
+
+# Dendrite
+dend = h.Section()
+dend.insert("pas")                # Passive channel
+dend.L = 100
+dend.diam = 1
+dend.g_pas = g_pas
+dend.insert("hh")
+
+# Spine neck needed probably!
+sn = h.Section()
+sn.insert("pas")                # Passive channel
+sn.L = 1
+sn.diam = 0.1
+sn.g_pas = g_pas
+sn.connect(dend, 0.5, 0)
+
+# Spine Head
+sh = h.Section()
+sh.insert("pas")                # Passive channel
+# This gives volume of 0.1um3
+sh.L = 0.2
+sh.diam = 0.8
+#h.dt = 0.001
+sh.g_pas = g_pas
+sh.connect(sn, 1, 0)
+
+# Synapse
+synstim = h.NetStim()
+synstim.start = 20
+#synstim.start = t_equil*1000 + 10
+synstim.noise = 0
+synstim.number = 10
+synstim.interval = 25
+
+ampasyn     = h.AmpaSyn(sh(0.5))
+ampanetcon  = h.NetCon(synstim, ampasyn)
+ampanetcon.weight[0] = 0.20E-3     # From the ddsp work
+
+nmdasyn     = h.NmdaSynUrak(sh(0.5))
+h.K0_NmdaSynUrak =  2.57                   
+h.delta_NmdaSynUrak = 0.96
+nmdanetcon  = h.NetCon(synstim, nmdasyn)
+nmdanetcon.weight[0] = 0.045E-3     # From the ddsp work
+
+## Used to initiate action potential
+apinit = h.ExpSyn(dend(0))
+apinit.tau = 0.3/3
+apinit.e = 0
+apinitcon = h.NetCon(synstim, apinit)
+apinitcon.weight[0] = 3*0.4/70 # 0.4nA/70mV = 0.4/70 uS
+apinitcon.delay = 1
+
+## This setting of parameters gives a calcium influx and pump
+## activation that is more-or-less scale-independent
+vol = sh.L*numpy.pi*(sh.diam/2)**2
+N_A = 6.02205E23 # Avogadro's constant
+# Concentration of one agent in the volume in mM 
+agconc = 1E18/(N_A * vol)
+
+## Reaction-diffusion mechanism
+## This appears to integrate the incoming Ca
+# WHERE the dynamics will take place
+r = rxd.Region([sh], nrn_region='i')
+
+# WHO are the actors
+ca        = rxd.Species(r, name='ca'       , charge=2, initial=0.001)
+Glu       = rxd.Species(r, name='Glu'   , charge=1, initial=0)
+NMDA      = rxd.Species(r, name='NMDA'     , charge=0, initial=3*agconc)
+CB        = rxd.Species(r, name='CB'       , charge=0, initial=0.100) # Faas &al
+cam       = rxd.Species(r, name='CaM'      , charge=0, initial=0.030) # Fass &al, Pepke &al
+CaMKII    = rxd.Species(r, name='CaMKII'   , charge=0, initial=0.080) # Pepke &al
+CaCB      = rxd.Species(r, name='CaCB'     , charge=0) 
+CaCaM2C   = rxd.Species(r, name='CaCaM2C'  , charge=0)
+CaCaM2N   = rxd.Species(r, name='CaCaM2N'  , charge=0)
+KCaCaM2C  = rxd.Species(r, name='KCaCaM2C' , charge=0)
+
+#  KCaCaM2C, , CaCB
+kappa = rxd.Kappa([ca, Glu, NMDA, CB, cam, CaMKII, CaCB, CaCaM2C, CaCaM2N, KCaCaM2C], "simple-psd-pepke-kappa-nmda.ka", r, time_units="ms", verbose=True)
+rxd.rxd.verbose=False
+## This setting of parameters gives a calcium influx and pump
+## activation that is more-or-less scale-independent
+vol = sh.L*numpy.pi*(sh.diam/2)**2
+
+## Reaction-diffusion mechanism
+## This appears to integrate the incoming Ca
+# WHERE the dynamics will take place
+#r = rxd.Region([sh], nrn_region='i')
+
+# WHO are the actors
+#ca = rxd.Species(r, name='ca', charge=2, initial=0.01)
+
+## Record Time from NEURON (neuron.h._ref_t)
+rec_t = h.Vector()
+rec_t.record(h._ref_t)
+## Record Voltage from the center of the soma
+rec_v = h.Vector()
+rec_v.record(sh(0.5)._ref_v)
+## Record Ca from spine head
+rec_cai = h.Vector()
+rec_cai.record(sh(0.5)._ref_cai)
+## Record ica from spine head
+rec_ica = h.Vector()
+rec_ica.record(sh(0.5)._ref_ica)
+## Record Ca bound to CB from spine head
+rec_CaCBi = h.Vector()
+rec_CaCBi.record(sh(0.5)._ref_CaCBi)
+## Record Free CaM from spine head
+rec_cami = h.Vector()
+rec_cami.record(sh(0.5)._ref_CaMi)
+
+rec_CaMKIIi = h.Vector()
+rec_CaMKIIi.record(sh(0.5)._ref_CaMKIIi)
+rec_CaCaM2Ni = h.Vector()
+rec_CaCaM2Ni.record(sh(0.5)._ref_CaCaM2Ni)
+rec_CaCaM2Ci = h.Vector()
+rec_CaCaM2Ci.record(sh(0.5)._ref_CaCaM2Ci)
+rec_KCaCaM2Ci = h.Vector()
+rec_KCaCaM2Ci.record(sh(0.5)._ref_KCaCaM2Ci)
+
+## Run
+init()
+print("Running kappa-only to initialise")
+## FIXME: put in some read-out to check when system has equilibriated.
+# kappa.run_free(120*1000)
+kappa.run_free(1000)
+# for i in range(1,t_equil):
+#     run(h.t + 10)
+#     kappa.run_free(990)
+#     h.t = h.t + 990
+print("Running NEURON-kappa")
+run(300)
+if (0):
+    for i in range(1,60):
+        print("Running kappa-only")
+        kappa.run_free(990)
+        print("Running NEURON-kappa and trying to trick NEURON")
+        h.t = h.t + 990
+        run(h.t + 10)
+
+## Plot
+import matplotlib.pyplot as plt
+
+## Get values from NEURON-vector format into Python format
+times = [] # Use list to add another trace later.
+voltages = []
+times.append(list(rec_t)) # alternative to `list(rec_t)`:
+                          # `numpy.array(rec_t)`
+voltages.append(list(rec_v))
+
+# Plot the recordings with matplotlib
+# ===================================
+
+import matplotlib.pyplot as plt
+
+# get values from NEURON-vector format into Python format
+times = [] # Use list to add another trace later.
+voltages = []
+cai = []
+ica = []
+cami = []
+
+times.append(list(rec_t)) # alternativ to `list(rec_t)`: `numpy.array(rec_t)`
+voltages.append(list(rec_v))
+cai.append(list(rec_cai))
+ica.append(list(rec_ica))
+cami.append(list(rec_cami))
+
+# check types by:
+# >>> type(rec_t)
+# >>> type(time[0])
+
+def plot_data(tmax=None):
+    if (tmax == None): 
+        tmax = max(times[0])
+    
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4, ncols=1)
+    ax1.plot(times[0], voltages[0])
+    ax1.set_xlabel("Time [ms]")
+    ax1.set_ylabel("V [mV]")
+    ax1.axis(ymin=-80, ymax=50)
+    ax1.axis(xmin=0, xmax=tmax)
+
+    ax2.plot(times[0], ica[0])
+    ax2.set_xlabel("Time [ms]")
+    ax2.set_ylabel("ICa [mA/cm2]")
+    ## ax2.axis(ymin=-1, ymax=0.1)
+    ax2.axis(xmin=0, xmax=tmax)
+
+    ax3.plot(times[0], cai[0])
+    ax3.plot(times[0], rec_CaCBi)
+    ax3.plot(times[0], cami[0])
+    ax3.plot(times[0], rec_KCaCaM2Ci)
+    ax3.set_xlabel("Time [ms]")
+    ax3.set_ylabel("[mM]")
+    plt.axes(ax3)
+    plt.legend(('Ca', 'CaCB', 'CaM', 'KCaCaM2C'))
+    ## ax3.axis(ymin=-1E-2, ymax=0.5E-1)
+    ax3.axis(xmin=0, xmax=tmax)
+
+    ax4.plot(times[0], rec_CaCaM2Ni)
+    ax4.plot(times[0], rec_CaMKIIi)
+    ax4.set_xlabel("Time [ms]")
+    ax4.set_ylabel("[mM]")
+    plt.axes(ax4)
+    plt.legend(('CaCaM2N', 'CaMKII'))
+    ax4.axis(ymin=-1E-5, ymax=1E-2)
+    ax4.axis(xmin=0, xmax=tmax)
+
+    fig.show() # If the interpreter stops now: close the figure.
+    # For interactive plotting, see `Part 1` -> `ipython`
+
+    fig.savefig("../doc/simple-psd-pepke-kappa-nmda.pdf", format='pdf')
+
+numpy.savez("simple-psd-pepke-kappa-nmda", t=times[0], cai=cai[0], cami=cami[0], ica=ica[0], voltages=voltages[0], diam=sh.diam)
+
+plot_data()
