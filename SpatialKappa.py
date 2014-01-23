8b4cc4e5299ede8a609f91538aaec7958b3a2f55
David C Sterratt <david.c.sterratt@ed.ac.uk>
2014-01-23 16:59:10 +0000

    Added classpath argument to constructor

--- SpatialKappa.py
+++ SpatialKappa.py
@@ -1,4 +1,5 @@
 import py4j.java_gateway as jg
+import os
 
 class SpatialKappa:
     """Runs gateway to SpatialKappa"""
@@ -7,14 +8,17 @@ class SpatialKappa:
     gateway = None
     gateway_client = None
 
-    def __init__(self):
-        self.port = jg.launch_gateway(classpath='.:../mlm/SpatialKappa/SpatialKappa-v2.1.1.jar', die_on_exit=True)
+    def __init__(self, classpath=''):
+        classpath = os.pathsep.join((classpath, os.pathsep.join(('.', '../mlm/SpatialKappa/SpatialKappa-v2.1.1.jar'))))
+        print classpath
+        self.port = jg.launch_gateway(classpath=classpath, die_on_exit=True)
         self.gateway_client = jg.GatewayClient(port=self.port)
         self.gateway = jg.JavaGateway(self.gateway_client)        
         jg.java_import(self.gateway.jvm, 'SpatialKappaSim.*')
         print self.gateway
 
     def kappa_sim(self, time_units, verbose):
-        return self.gateway.jvm.SpatialKappaSim(time_units, verbose)
+        ks = self.gateway.jvm.SpatialKappaSim(time_units, verbose)
+        return ks
 
 
