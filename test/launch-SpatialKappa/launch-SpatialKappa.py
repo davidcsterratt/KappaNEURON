f31aa6706de0eedd5bb78161ce087c6b4da5e6d3
David C Sterratt <david.c.sterratt@ed.ac.uk>
2014-01-23 16:59:55 +0000

    Test now runs Kappa file

--- test/launch-SpatialKappa/launch-SpatialKappa.py
+++ test/launch-SpatialKappa/launch-SpatialKappa.py
@@ -1,6 +1,8 @@
 from SpatialKappa import *
 
-sk = SpatialKappa()
-sk.kappa_sim("ms", True)
-
-
+sk = SpatialKappa(classpath=os.pathsep.join(('../..', '../../../mlm/SpatialKappa/SpatialKappa-v2.1.1.jar', '../../../mlm/SpatialKappa/ant-antlr-3.2.jar')))
+kappa_sim = sk.kappa_sim("ms", True)
+kappa_sim.loadFile("caPump.ka")
+kappa_sim.runToTime(0.01)
+print kappa_sim.getObservation("P")
+print kappa_sim.getObservation("ca")
