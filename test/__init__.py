e9b23996a7b3d619f37f207910bfd74c73ab5d17
David C Sterratt <david.c.sterratt@ed.ac.uk>
2014-08-05 16:27:01 +0100

    Added backtraces from exceptions

--- test/__init__.py
+++ test/__init__.py
@@ -4,8 +4,22 @@ import unittest
 from neuron import *
 from neuron import rxd
 
+import functools
+def debug_on(*exceptions):
+    if not exceptions:
+        exceptions = (AssertionError, )
+    def decorator(f):
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except exceptions:
+                pdb.post_mortem(sys.exc_info()[2])
+        return wrapper
+    return decorator
+
 class TestKappaNEURON(unittest.TestCase):
-    
+    @debug_on()
     def setUp(self):
         pass
 
