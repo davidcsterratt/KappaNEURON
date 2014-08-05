03b6668458ee46036aced4ae89bf6e76dc743a85
David C Sterratt <david.c.sterratt@ed.ac.uk>
2014-08-05 10:35:08 +0100

    Start of unittest framework

---
+++ test/__init__.py
@@ -0,0 +1,26 @@
+import KappaNEURON
+import os
+import unittest
+from neuron import *
+from neuron import rxd
+
+class TestKappaNEURON(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+    def test_createSection(self):
+        sh = h.Section()
+        self.assertIsInstance(sh, nrn.Section)
+
+    def test_injectCalcium(self):
+        sh = h.Section()
+        r = rxd.Region([sh], nrn_region='i')
+        ca = rxd.Species(r, name='ca', charge=2, initial=0.0)
+        kappa = KappaNEURON.Kappa([ca], self.__module__ + "/caMinimal.ka", r, verbose=True)
+
+    def tearDown(self):
+        pass
+        
+if __name__ == '__main__':
+    unittest.main()
