64e9bd66b3a86c95e9c6805b1a403b04e9221712
David C Sterratt <david.c.sterratt@ed.ac.uk>
2014-08-06 17:25:20 +0100

    Basis for multiple tests now working

--- test/__init__.py
+++ test/__init__.py
@@ -7,29 +7,38 @@ from neuron import rxd
 
 class TestKappaNEURON(unittest.TestCase):
     def setUp(self):
-        pass
+        self.sh = h.Section()
+        self.sh.insert("capulse")
+        self.sh.L=2
+        self.sh.diam=1
+        self.r = rxd.Region([self.sh], nrn_region='i')
+        if 'ca' not in rxd.species._get_all_species():
+            self.ca = rxd.Species(self.r, name='ca', charge=2, initial=0.0)
+        else:
+            self.ca = rxd.species._get_all_species()['ca']()
+        self.kappa = KappaNEURON.Kappa([self.ca], self.__module__ + "/caMinimal.ka", self.r, verbose=True)
+        KappaNEURON.progress = False
+        self.rec_cai = h.Vector()
+        self.rec_cai.record(self.sh(0.5)._ref_cai)
 
     def test_createSection(self):
-        print 'test_createSection'
-        sh = h.Section()
-        self.assertIsInstance(sh, nrn.Section)
-        sh.insert("capulse")
+        self.assertIsInstance(self.sh, nrn.Section)
+        init()
+        self.assertEqual(h.t, 0.0)
+        self.assertEqual(self.sh(0.5).cai, 0.0)
 
     def test_injectCalcium(self):
-        print 'test_injectCalcium'
-        sh = h.Section()
-        sh.insert("capulse")
-        sh.L=2
-        sh.diam=1
-        r = rxd.Region([sh], nrn_region='i')
-        ca = rxd.Species(r, name='ca', charge=2, initial=0.0)
-        kappa = KappaNEURON.Kappa([ca], self.__module__ + "/caMinimal.ka", r, verbose=True)
+        init()
+        self.assertEqual(h.t, 0.0)
+        # self.assertEqual(self.sh(0.5).cai, 0.0)
+        run(30)
+
+    def test_injectCalcium2(self):
         init()
         run(30)
-        kappa = []
 
     def tearDown(self):
-        pass
+        self.kappa = None
         
 if __name__ == '__main__':
     unittest.main()
