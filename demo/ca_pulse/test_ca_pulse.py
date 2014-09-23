ccd9bf745ce96fa9059ef137ed20cf9c44d2f7dd
David C Sterratt <david.c.sterratt@ed.ac.uk>
2014-09-23 17:02:25 +0100

    Tidied and commmented

--- demo/ca_pulse/test_ca_pulse.py
+++ demo/ca_pulse/test_ca_pulse.py
@@ -9,32 +9,37 @@ def run(diam=0.2,
         gamma2=1,
         P0=0.2,
         vclamp=False):
-    # Neuron spine head
+    # Make spine head in NEURON
     sh = make_spine_head(diam=diam, gcalbar=gcalbar)
 
-    ## Reaction-diffusion mechanism
-    ## This appears to integrate the incoming Ca
-    # WHERE the dynamics will take place
+    ## Create region where the dynamics will take place
     r = rxd.Region([sh], nrn_region='i')
 
-    # WHO are the actors
+    ## Create species
+    # Calcium
     ca = rxd.Species(r, name='ca', charge=2, initial=0.0)
+
+    # Pump molecule
     P  = rxd.Species(r, name='P',  charge=0, initial=P0)
-    # kappa = KappaNEURON.Kappa([ca, P], "caPump.ka", r, verbose=True, mode='continuous_influx')
+
+    ## Create Kappa simulation defined in caPump.ka in the context of
+    ## the spine. Since Calcium crosses the membrane it is given in
+    ## the membrane_species argument, whereas the pump molecule is
+    ## defined in the species argument as it is purely internal. 
     KappaNEURON.mode='continuous_influx'
-    kappa = KappaNEURON.Kappa([ca, P], "caPump.ka", r, verbose=True)
+    kappa = KappaNEURON.Kappa(membrane_species=[ca], species=[P], kappa_file='caPump.ka', regions=r)
+    ## Set variables in the Kappa simulation
     vol = sh.L*numpy.pi*(sh.diam/2)**2
-    ## kappa.setVariable('gamma1', 1E-3*0.1*numpy.pi*0.5**2/vol)
-    ## kappa.setVariable('gamma2', gamma2)
     kappa.setVariable('k1', 47.3)
     kappa.setVariable('k2', gamma2)
     kappa.setVariable('vol', vol)
 
+    ## Insert Vclamp if desired
     if vclamp:
         stim = insert_vclamp(sh)
 
     ## Record P from spine head
     rec_Pi = h.Vector()
     rec_Pi.record(sh(0.5)._ref_Pi)
-
+    
     run_and_save(sh, rec_Pi, 'test_ca_pulse')
